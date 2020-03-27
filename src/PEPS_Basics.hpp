8cde80a62b5a9f43f0223b6fd54b0250b663b168
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-03-27 21:10:03 +0900

    added license information header

--- src/PEPS_Basics.hpp
+++ src/PEPS_Basics.hpp
@@ -1,12 +1,30 @@
-#ifndef _PEPS_BASICS_HPP_
-#define _PEPS_BASICS_HPP_
+/* TeNeS - Massively parallel tensor network solver /
+/ Copyright (C) 2019- The University of Tokyo */
+
+/* This program is free software: you can redistribute it and/or modify /
+/ it under the terms of the GNU General Public License as published by /
+/ the Free Software Foundation, either version 3 of the License, or /
+/ (at your option) any later version. */
+
+/* This program is distributed in the hope that it will be useful, /
+/ but WITHOUT ANY WARRANTY; without even the implied warranty of /
+/ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the /
+/ GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License /
+/ along with this program. If not, see http://www.gnu.org/licenses/. */
 
 /*
+ *
  Basic routines independent on unit cell structures.
  Using mptensor libraries
  (Test version)
  2015 Dec.  Tsuyoshi Okubo
 */
+
+#ifndef _PEPS_BASICS_HPP_
+#define _PEPS_BASICS_HPP_
+
 
 #include <cmath>
 #include <cstdio>
