66dd6256a23f0deb57553f2507f52b16cd05ef56
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-03-27 17:02:08 +0900

    bcast string parameters (such as tensor_save)

--- src/mpi.cpp
+++ src/mpi.cpp
@@ -1,6 +1,9 @@
+#include <cstring>
+
 #include "mpi.hpp"
 
 #ifdef _NO_MPI
+
 int MPI_Init(int*, char***) { return 0; }
 int MPI_Barrier(MPI_Comm) { return 0; }
 int MPI_Finalize() { return 0; }
@@ -36,4 +39,44 @@ int bcast(bool &val, int root, MPI_Comm comm){
   return ret;
 }
 
+int bcast(std::string &val, int root, MPI_Comm comm){
+  int ret=0;
+#ifndef _NO_MPI
+  int mpirank;
+  MPI_Comm_rank(comm, &mpirank);
+  int sz = val.size()+1;
+  bcast(sz, root, comm);
+  char* buf = new char[sz];
+  if(mpirank==root){
+    std::strncpy(buf, val.c_str(), sz);
+  }
+  ret = MPI_Bcast(buf, sz, MPI_CHAR, root, comm);
+  if(mpirank!=root){
+    val = buf;
+  }
+  delete buf;
+#endif
+  return ret;
 }
+
+int bcast(std::vector<std::string> &val, int root, MPI_Comm comm){
+  int ret=0;
+#ifndef _NO_MPI
+  int mpirank;
+  MPI_Comm_rank(comm, &mpirank);
+  int sz = val.size();
+  bcast(sz, root, comm);
+  if(mpirank!=root){
+    val.resize(sz);
+  }
+  for(int i=0; i<sz; ++i){
+    int r=bcast(val[i], root, comm);
+    if(r!=0){
+      ret = r;
+    }
+  }
+#endif
+  return ret;
+}
+
+} // end of namespace tenes
