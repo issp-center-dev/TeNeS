33dcb1f7d353f41c1a790b95851a9a62e0839002
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-11-08 21:42:59 +0900

    update tutorial-1

--- sample/01_transverse_field_ising/tutorial_example.py
+++ sample/01_transverse_field_ising/tutorial_example.py
@@ -1,9 +1,9 @@
 import subprocess
 
 import numpy as np
-
 import toml
 
+MPI_cmd = "mpiexec -np 2"  # e.g., "mpiexec -np 1"
 
 num_hx = 16
 min_hx = 0.0
@@ -11,16 +11,16 @@ max_hx = 3.0
 
 total = 0
 for idx, hx in enumerate(np.linspace(min_hx, max_hx, num=num_hx)):
-    print("Caclulation Process: {}/{}".format(idx+1, num_hx))
+    print("Caclulation Process: {}/{}".format(idx + 1, num_hx))
     with open("simple.toml") as f:
         dict_toml = toml.load(f)
     dict_toml["parameter"]["general"]["output"] = "output_{}".format(idx)
     dict_toml["model"]["hx"] = float(hx)
-    with open("simple_{}.toml".format(idx), 'w') as f:
+    with open("simple_{}.toml".format(idx), "w") as f:
         toml.dump(dict_toml, f)
     cmd = "tenes_simple simple_{}.toml -o std_{}.toml".format(idx, idx)
     subprocess.call(cmd.split())
     cmd = "tenes_std std_{}.toml -o input_{}.toml".format(idx, idx)
     subprocess.call(cmd.split())
-    cmd = "tenes input_{}.toml".format(idx)
+    cmd = "{} tenes input_{}.toml".format(MPI_cmd, idx)
     subprocess.call(cmd.split())
