7296850a85d1d105aeb04fe63e87667e6bcdad8d
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2023-06-14 16:56:41 +0900

    update sample

--- sample/07_timeevolution/run.sh
+++ sample/07_timeevolution/run.sh
@@ -1,12 +1,12 @@
-# obtain ground state before quency
+# obtain ground state before quench
 tenes_simple simple.toml
 tenes_std std.toml
 tenes input.toml
 
-for name in weak strong; do
+for name in weak middle strong; do
   tenes_simple -o std_te_${name}.toml simple_te_${name}.toml
   tenes_std -o input_te_${name}.toml std_te_${name}.toml
   tenes input_te_${name}.toml
 
-  awk '$2 == 2 {print $1, $3, $4}' output_te_${name}/TE_density.dat > magnetization_${name}.dat
+  awk '$2 == 1 {print $1, $3, $4}' output_te_${name}/TE_density.dat > magnetization_${name}.dat
 done
