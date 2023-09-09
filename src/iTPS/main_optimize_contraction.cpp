b7f02a98c4028d3f64536a1799399759041053e4
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2023-09-10 00:11:41 +0900

    change filename

--- src/iTPS/main_optimize_contraction.cpp
+++ src/iTPS/main_optimize_contraction.cpp
@@ -14,8 +14,6 @@
 /* You should have received a copy of the GNU General Public License /
 / along with this program. If not, see http://www.gnu.org/licenses/. */
 
-// #include "main.hpp"
-
 #include <complex>  // for complex
 #include <cstdlib>  // for abs, size_t
 #include <set>
