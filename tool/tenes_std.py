3fba2c276000daf5fb418594a2ba25a0704ac00d
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2023-11-01 15:42:10 +0900

    change version number to 2.0-dev

--- tool/tenes_std.py
+++ tool/tenes_std.py
@@ -1133,7 +1133,7 @@ if __name__ == "__main__":
         "-o", "--output", dest="output", default="input.toml", help="Output TOML file"
     )
     parser.add_argument(
-        "-v", "--version", dest="version", action="version", version="2.0-beta"
+        "-v", "--version", dest="version", action="version", version="2.0-dev"
     )
 
     args = parser.parse_args()
