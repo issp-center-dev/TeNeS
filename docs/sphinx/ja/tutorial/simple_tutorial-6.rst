4244fc7158e16b986eeb48470ef6c3b4744b60c5
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2023-11-02 10:51:10 +0900

    update tutorial_6

--- docs/sphinx/ja/tutorial/simple_tutorial-6.rst
+++ docs/sphinx/ja/tutorial/simple_tutorial-6.rst
@@ -15,16 +15,20 @@
 ここで\ :math:`\langle i, j\rangle`\ は隣接サイトの組を表し、\ :math:`\mu`\ は化学ポテンシャル、\ :math:`t`\ はホッピングエネルギー、\ :math:`V`\ は隣接サイト間の相互作用の大きさを表します。
 ハードコアボゾン模型では、各サイトのボソン数は0か1に制限されます。
 この模型では、2つのタイプの長距離秩序によって特徴づけられるいくつかの秩序相が現れることが知られています :ref:`[Wessel] <Ref-Wessel>` 。
-1/3フィリングでは、 :numref:`fig_tutorial6_hardcore_boson` の挿入図に示すような\ :math:`\sqrt{3}\times\sqrt{3}`\ 超格子構造をもつ固体相が出現します。これは波数\ :math:`\boldsymbol{Q}=(4\pi/3,0)`\の構造因子\ :math:`S(\boldsymbol{Q})`\を計算することで特徴づけることができます。
-もう一つの秩序は消滅演算子の期待値の大きさ\ :math:`|\langle b \rangle|`\で特徴づけられる超流動秩序です。
+まずは消滅演算子の期待値の大きさ\ :math:`|\langle b \rangle|`\で特徴づけられる超流動秩序です。
+もうひとつは固体秩序です。 1/3フィリングでは、 :numref:`fig_tutorial6_hardcore_boson` の挿入図に示すような\ :math:`\sqrt{3}\times\sqrt{3}`\ 超格子構造をもつ固体相が出現します。
+これは波数\ :math:`\boldsymbol{Q}=(4\pi/3,0)`\の構造因子\ :math:`S(\boldsymbol{Q}) = \sum_{ij}^{N_\text{sites}} \langle n_i n_j \rangle \exp[-i\boldsymbol{Q}\cdot(r_i - r_j)] / N_\text{site}`\ を計算することで特徴づけることができます。
 
-この模型の計算を行うには、 ``sample/06_hardcore_boson_triangular`` ディレクトリ中にある toml ファイル ``basic.toml`` , ``nn_obs.toml`` と、Pythonスクリプト ``run.py`` を利用します。 ``basic.toml`` ファイルには模型の設定やパラメータなどが記述されています。このファイルの記述は、前節の三角格子ハイゼンベルク模型とほぼ同じであるため、内容は割愛します。唯一の変更点は最後の ``model`` セクションだけです。ここで、 ``type = "boson"`` によってハードコアボゾン模型を指定しており、 ``t = 0.1`` , ``V = 1`` によってホッピングおよび隣接サイト間相互作用の大きさを指定しています。
+この模型の計算を行うには、 ``sample/06_hardcore_boson_triangular`` ディレクトリ中にある toml ファイル ``basic.toml``, ``nn_obs.toml`` と、Pythonスクリプト ``run.py`` を利用します。 ``basic.toml`` ファイルには模型の設定やパラメータなどが記述されています。このファイルの記述は、前節の三角格子ハイゼンベルク模型とほぼ同じであるため、内容は割愛します。唯一の変更点は最後の ``model`` セクションだけです。ここで、 ``type = "boson"`` によってハードコアボゾン模型を指定しており、 ``t = 0.1``, ``V = 1`` によってホッピングおよび隣接サイト間相互作用の大きさを指定しています。
 
-``nn_obs.toml`` ファイルには計算する構造因子の情報が記述されています。
+構造因子 :math:`S(\boldsymbol{Q})` を計算するためには、（ユニットセル中の）全サイト対における密度密度相関 :math:`\langle n_i n_j \rangle` を計算する必要があります。
+この演算子は ``tenes_simple`` では定義されないため、別途定義する必要があります。
+``nn_obs.toml`` ファイルに :math:`3 \times 3` ユニットセルにおける演算子が定義されており、 ``run.py`` で ``tenes_std`` の入力ファイル ``std_XXX_YYY.toml`` に追記しています。
 
-.. literalinclude:: ../../../../sample/06_hardcore_boson_triangular/nn_obs.toml
-
-このファイルによって、構造因子\ :math:`S(\boldsymbol{Q})`\を計算することができます。
+より大きなユニットセルを用いる場合には、密度密度相関を計算するコストが非常に大きくなってしまい、構造因子を計算することが現実的ではなくなります。
+一方、TeNeSは（正方格子iTPSの）x軸、y軸方向の相関関数を低コストで計算できます。これを利用して長距離秩序の有無を確認できます。
+また、1体の密度演算子の期待値 :math:`\langle n_i \rangle` のフーリエ変換 :math:`n(\boldsymbol{Q})` も使えます。
+1/3フィリングの基底状態は3重縮退していますが、有限ボンド次元計算ではそのうちのどれかが選ばれます。その結果、得られる密度はサイト依存性があります。
 
 実際にスクリプト ``run.py`` を使って計算を実行してみましょう。
 あらかじめ ``tenes`` などにパスを通した上で
@@ -40,16 +44,17 @@
 
     load 'plot.gp'
 
-とすれば、 :numref:`fig_tutorial6_hardcore_boson` (a)のような構造因子\ :math:`S(\boldsymbol{Q})`\と超流動秩序パラメータ\ :math:`|\langle b \rangle|`\のグラフが得られます。
+とすれば、 :numref:`fig_tutorial6_hardcore_boson` のようなグラフが得られます。
+:math:`S(\boldsymbol{Q})` がユニットセル内の密度相関関数から計算した構造因子、 :math:`S'(\boldsymbol{Q})` がx方向の相関関数から計算した構造因子、 :math:`n(\boldsymbol{Q})` が密度のフーリエ変換、 :math:`(|\langle b \rangle| + |\langle b^\dagger \rangle|)/2` が超流動秩序パラメータです。
 なお、ここで計算に用いたボンド次元は小さい(ボンド次元2)ため、計算精度はそれほど高くありません。
-スクリプト ``run.py`` の冒頭でコメントになっている4行をコメントアウトすることで、時間がかかりますが、より高精度の計算を行うこともできます。その結果を :numref:`fig_tutorial6_hardcore_boson` (b)に示します。
+スクリプト ``run.py`` の冒頭でボンド次元を増やすことで、時間がかかりますが、より高精度の計算を行うこともできます。
 この図から、系の基底状態は3種類の相、つまり(a) 超流動相(\ :math:`-0.5 \lesssim \mu/V \lesssim -0.2`\), (b) 固体相(\ :math:`-0.2 \lesssim \mu/V \lesssim 2.4`\), および(c) 超流動固体相(\ :math:`2.4 \lesssim \mu/V`\)が現れることがわかります。これは先行研究の結果と整合します :ref:`[Wessel] <Ref-Wessel>` 。
 
 .. figure:: ../../img/tutorial_6_hardcore_boson.*
 	:name: fig_tutorial6_hardcore_boson
 	:width: 600px
 
-	三角格子上のハードコアボゾン模型の基底状態相図. (a) ボンド次元が2のとき. (b) ボンド次元が5のとき. 挿入図: 固体相のときの粒子配置. 
+	三角格子上のハードコアボゾン模型の基底状態相図.
 
 .. rubric:: 参考文献
 
--- sample/06_hardcore_boson_triangular/run.py
+++ sample/06_hardcore_boson_triangular/run.py
@@ -1,31 +1,53 @@
+from __future__ import annotations
 import subprocess
+from typing import TextIO
 from os.path import join
 import numpy as np
 import toml
 
 MPI_cmd = ""  # mpiexec -np 4"
 
-do_calculation = True
-decrease_mu = True # sweep mu from larger to smaller or not
+# calculate S(q) = \sum <n_i n_j> exp[-i q(r_i - r_j)] or not
+# If True, only 3x3 unitcell is available
+calculate_sq = True
 
-num_h = 21
-min_h = -1.0
-max_h = 3.0
+# if False, skip computing and only gather results into *.dat (e.g., nq.dat)
+do_calculation = True
+
+# sweep mu from larger to smaller or not
+decrease_mu = True
+
+num_mu = 21
+min_mu = -1.0
+max_mu = 3.0
 num_step_table = [1000, 2000]
 D = 2
 chi = 4
+
 # Please uncomment the following lines if you hope to obtain more accurate results
-# num_h = 41
+# num_mu = 41
 # num_step_table = [1500, 3000]
 # D = 5
 # chi = 25
 
-fsq = open("sq.dat", "w")
-fmag0 = open("offdiag.dat", "w")
-fmag = open("density.dat", "w")
-fene = open("energy.dat", "w")
+# file objects for physical quantities
+files: dict[str, TextIO] = {}
 
-for f in (fsq, fmag, fmag0, fene):
+# N(q) = (1/Nsite) \sum_i <n_i> exp[-i q r_i] with q = (4pi/3, 0)
+files["nq"] = open("nq.dat", "w")
+if calculate_sq:
+    # S(q) = (1/Nsite) \sum_{ij} <n_i n_j> exp[-i q (r_i-r_j)] with q = (4pi/3, 0)
+    files["sq"] = open("sq.dat", "w")
+# S(q)' = (1/Nsite) \sum_i \sum_dx <n_i n(r_i+dx)> exp[-i q dx] with q = (4pi/3, 0)
+files["sq_x"] = open("sq_x.dat", "w")
+# N0 = (<B> + <B^dagger>) / 2
+files["den0"] = open("offdiag.dat", "w")
+# N = (1/Nsite) \sum_i <n_i>
+files["den"] = open("density.dat", "w")
+# E = <H>
+files["ene"] = open("energy.dat", "w")
+
+for f in files.values():
     f.write("# $1: mu\n")
     for i, num_step in enumerate(num_step_table, 2):
         f.write(f"# ${i}: num_step={num_step}\n")
@@ -34,14 +56,12 @@ for f in (fsq, fmag, fmag0, fene):
 
 step_mu = -1 if decrease_mu else 1
 
-for idx, h in enumerate(np.linspace(min_h, max_h, num=num_h)[::step_mu]):
-    print(f"Calculation Process: {idx+1}/{num_h}")
+for idx, mu in enumerate(np.linspace(min_mu, max_mu, num=num_mu)[::step_mu]):
+    print(f"Calculation Process: {idx+1}/{num_mu}")
     inum = 0
     num_pre = 0
-    fsq.write("{} ".format(h))
-    fmag0.write("{} ".format(h))
-    fmag.write("{} ".format(h))
-    fene.write("{} ".format(h))
+    for f in files.values():
+        f.write("{} ".format(mu))
     for num_step in num_step_table:
         ns = num_step - num_pre
         print(f"Steps: {num_step}")
@@ -49,12 +69,21 @@ for idx, h in enumerate(np.linspace(min_h, max_h, num=num_h)[::step_mu]):
         if do_calculation:
             with open("basic.toml") as f:
                 dict_toml = toml.load(f)
+
+            L = dict_toml["lattice"]["L"]
+            W = dict_toml["lattice"]["W"]
+            nsites = L * W
+            if calculate_sq and nsites != 9:
+                raise RuntimeError("The number of sites is not 9 (3x3)!")
+
             dict_toml["parameter"]["general"]["output"] = output_dir
             dict_toml["parameter"]["general"]["tensor_save"] = "tensor_save"
-            dict_toml["model"]["mu"] = float(h)
+            dict_toml["model"]["mu"] = float(mu)
             dict_toml["lattice"]["virtual_dim"] = D
             dict_toml["parameter"]["ctm"]["dimension"] = chi
             dict_toml["parameter"]["simple_update"]["num_step"] = ns
+
+            # load the previous optimized tensor
             if idx > 0 or inum > 0:
                 dict_toml["parameter"]["general"]["tensor_load"] = "tensor_save"
 
@@ -67,73 +96,120 @@ for idx, h in enumerate(np.linspace(min_h, max_h, num=num_h)[::step_mu]):
             cmd = f"tenes_simple {simple_toml} -o {std_toml}"
             subprocess.call(cmd.split())
 
-            with open(std_toml, "a") as fout:
-                with open("nn_obs.toml") as fin:
-                    for line in fin:
-                        fout.write(line)
+            if calculate_sq:
+                # append twosite observable <n_i n_j> to std.toml
+                with open(std_toml, "a") as f:
+                    with open("nn_obs.toml") as fin:
+                        for line in fin:
+                            f.write(line)
             cmd = f"tenes_std {std_toml} -o {input_toml}"
             subprocess.call(cmd.split())
 
             cmd = f"{MPI_cmd} tenes {input_toml}"
             subprocess.call(cmd.split())
-        ene = ""
-        density = ""
+
         with open(join(output_dir, "density.dat")) as f:
             for line in f:
+                if line.startswith("#"):
+                    continue
                 words = line.split("=")
                 if words[0].strip() == "N":
-                    density = words[1].strip()
+                    density = float(words[1].strip().split()[0])
+                    files["den"].write(f"{density} ")
                 if words[0].strip() == "Energy":
-                    ene = words[1].strip()
-        fene.write(f"{ene} ")
-        fmag.write(f"{density} ")
-        fene.flush()
-        fmag.flush()
+                    ene = float(words[1].strip().split()[0])
+                    files["ene"].write(f"{ene} ")
 
-        n0 = 0.0
-        N = np.zeros(9)
+        coords_list = []
+        with open("coordinates.dat") as f:
+            for line in f:
+                words = line.split()
+                if len(words) == 0:
+                    continue
+                if words[0].startswith("#"):
+                    continue
+                x = float(words[1])
+                y = float(words[2])
+                coords_list.append([x, y])
+        coords = np.array(coords_list)
+
+        nsites = coords.shape[0]
+        q = np.array([4 * np.pi / 3, 0])
+        nq_coeffs = np.cos(coords @ q)
+
+        n0 = 0.0  # (<B> + <B^dagger>) / 2
+        nq = 0.0  # n(q), q = (4pi/a, 0)
+        N = np.zeros(nsites)
         with open(join(output_dir, "onesite_obs.dat")) as f:
             for line in f:
+                if line.startswith("#"):
+                    continue
                 words = line.split()
                 if len(words) == 0:
                     continue
-                if words[0].strip() == "0":
+                if words[0].strip() == "0":  # N
+                    i = int(words[1])
                     n = float(words[2])
-                    N[int(words[1])] = n
-                if words[0].strip() == "1":
+                    nq += nq_coeffs[i] * n
+                    N[i] = n
+                if words[0].strip() == "1":  # Bdagger
                     n = float(words[2])
-                    n0 += np.abs(n) / 18.0
-                if words[0].strip() == "2":
+                    n0 += 0.5 * np.abs(n)
+                if words[0].strip() == "2":  # B
                     n = float(words[2])
-                    n0 += np.abs(n) / 18.0
-        fmag0.write("{} ".format(n0))
-        fmag0.flush()
+                    n0 += 0.5 * np.abs(n)
+        files["den0"].write(f"{n0 / nsites} ")
+        files["nq"].write(f"{nq / nsites} ")
+
+        if calculate_sq:
+            sq = 0.0  # S(q), q = (4pi/a, 0)
+            for i in range(nsites):
+                sq += N[i]  # \hat{n}_i^2 = \hat{n}_i for hardcore boson
+            with open(join(output_dir, "twosite_obs.dat")) as f:
+                for line in f:
+                    words = line.split()
+                    if len(words) == 0:
+                        continue
+                    if words[0].strip() == "4":  # <nn>
+                        dx = int(words[2])
+                        dy = int(words[3])
+                        nn = float(words[4])
+                        if dx == dy:
+                            sq += nn
+                        else:
+                            sq -= 0.5 * nn
+            files["sq"].write(f"{sq / nsites} ")
 
         sq = 0.0
-        for i in range(3):
+        for i in range(nsites):
+            # C(r=0)
+            # \hat{n}_i^2 = \hat{n}_i for hardcore boson
             sq += N[i]
-
-        with open(join(output_dir, "twosite_obs.dat")) as f:
+        with open(join(output_dir, "correlation.dat")) as f:
             for line in f:
+                if line.startswith("#"):
+                    continue
                 words = line.split()
                 if len(words) == 0:
                     continue
-                if words[0].strip() == "4":
-                    n = float(words[4])
-                    if words[2] != words[3]:
-                        n *= -0.5
-                    sq += n
-        sq *= 3.0 / 9 / 9
-        fsq.write(f"{sq} ")
-        fsq.flush()
+                dx = int(words[3])
+                dy = int(words[4])
+                nn = float(words[5])
+
+                # Consider only the C(r) along x-axis
+                if dy > 0:
+                    continue
+                if dx % 3 == 0:
+                    sq += nn
+                else:
+                    sq += -0.5 * nn
+        files["sq_x"].write(f"{sq / nsites} ")
 
         inum = inum + 1
         num_pre = num_step
-    fene.write("\n")
-    fmag.write("\n")
-    fmag0.write("\n")
-    fsq.write("\n")
-fene.close()
-fmag.close()
-fmag0.close()
-fsq.close()
+        for f in files.values():
+            f.flush()
+    for f in files.values():
+        f.write("\n")
+for f in files.values():
+    f.close()
