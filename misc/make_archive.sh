8274d494bca9d8533c5941c445b9c22d5a95b81d
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2023-11-17 14:21:38 +0900

    change version number to 2.1-dev

--- misc/make_archive.sh
+++ misc/make_archive.sh
@@ -27,7 +27,7 @@ for lang in ja en; do
 done
 cd $ROOT_DIR
 
-version=2.0.0
+version=2.1-dev
 
 git submodule update -i -r
 git-archive-all \
--- src/version.hpp
+++ src/version.hpp
@@ -17,6 +17,6 @@
 #ifndef TENES_SRC_VERSION_HPP_
 #define TENES_SRC_VERSION_HPP_
 
-#define TENES_VERSION "2.0.0"
+#define TENES_VERSION "2.1-dev"
 
 #endif // TENES_SRC_VERSION_HPP_
