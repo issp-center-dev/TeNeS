ab2650b5ee3bfe65744c626b7c289b833b8247f2
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2024-12-14 05:55:56 +0900

    update sample for finite_temprature calculation

--- sample/03_finite_temperature/QMC/exec.sh
+++ sample/03_finite_temperature/QMC/exec.sh
@@ -9,16 +9,16 @@ for L in 8 32; do
 
     dla_alg -l lattice_L${L}.toml -h hamiltonian_G${G}.toml
 
-    for T in 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.25 1.5 1.75 2.0 5.0 10.0; do
+    for beta in 0.2 0.4 0.6 0.8 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0 3.5 4.0; do
       cp common_param.in param.in
-      beta=`echo "1.0/$T" | bc -l`
       echo "beta = $beta" >> param.in
+      T=`echo "1.0/$beta" | bc -l`
       srun dla param.in
-      output=sample-L${L}-G${G}-T${T}.log
+      output=sample-L${L}-G${G}-beta${beta}.log
       mv sample.log $output
-      grep "ene" $output | awk --assign="T=$T" '{print T,$4,$5}' >> $file_ene
-      grep "bmzu" $output | awk --assign="T=$T" '{print T,$4,$5}' >> $file_magx
-      grep "spe" $output | awk --assign="T=$T" '{print T,$4,$5}' >> $file_spec
+      grep "ene" $output | awk --assign="beta=$beta" '{print beta,$4,$5}' >> $file_ene
+      grep "bmzu" $output | awk --assign="beta=$beta" '{print beta,$4,$5}' >> $file_magx
+      grep "spe" $output | awk --assign="beta=$beta" '{print beta,$4,$5}' >> $file_spec
     done
   done
 done
