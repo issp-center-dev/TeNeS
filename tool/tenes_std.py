06c2039e15548f7b22b409cfc804197579480f69
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2025-06-30 11:20:30 +0900

    v2.1.2

--- tool/tenes_std.py
+++ tool/tenes_std.py
@@ -1273,7 +1273,7 @@ if __name__ == "__main__":
         "-o", "--output", dest="output", default="input.toml", help="Output TOML file"
     )
     parser.add_argument(
-        "-v", "--version", dest="version", action="version", version="2.2-dev"
+        "-v", "--version", dest="version", action="version", version="2.1.2"
     )
 
     args = parser.parse_args()
